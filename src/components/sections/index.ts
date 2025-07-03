6923a4b82e387017bc2f711bec6642e592692f73
mnokerdev <mnoker.dev@gmail.com>
2025-07-03 00:06:59 -0700

    feat(section): add package section feat(icons): add package icon chore: remove timeout for InView animations chore(css): lighten surface colors

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,5 +2,6 @@ import Welcome from "./Welcome.astro";
 import Hero from "./Hero.astro";
 import Gallery from "./Gallery.astro";
 import Amenities from "./Amenities.astro";
+import Package from "./Package.astro";
 
-export { Welcome, Hero, Gallery, Amenities };
+export { Welcome, Hero, Gallery, Amenities, Package };
